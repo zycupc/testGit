6b0db9e6c361568cb66c096134c4590e45b62ddc
zycupc <zycupc@163.com>
2020-05-06 20:27:40 +0800

    第二次修改

--- src/com/zyc/demo/Test01.java
+++ src/com/zyc/demo/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("testGit");
         System.out.println("修改一次");
+        System.out.println("第二次修改");
     }
 }
